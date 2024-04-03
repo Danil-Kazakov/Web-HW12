805119d0e1d2de20f35c96360509a2cbdf8c0231
Danil-Kazakov <143808671+Danil-Kazakov@users.noreply.github.com>
2024-04-03 20:17:36 +0300

    Add files via upload

---
+++ Web-HW11/routes.py
@@ -0,0 +1,90 @@
+from fastapi import APIRouter, HTTPException, Depends, status, Security
+from fastapi.security import (
+    OAuth2PasswordRequestForm,
+    HTTPAuthorizationCredentials,
+    HTTPBearer,
+)
+from sqlalchemy.orm import Session
+from db import get_db
+import repository as repository_contacts
+from schemas import ContactResponse, UserResponse, UserModel, TokenModel
+from typing import List
+from auth import auth_service
+import repository as repository_users
+
+app = APIRouter(prefix='/contacts', tags=["contacts"])
+router = APIRouter(prefix='/auth', tags=["auth"])
+security = HTTPBearer()
+
+
+@app.get('/', response_model=List[ContactResponse])
+async def read_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    contacts = await repository_contacts.get_contacts(skip, limit, db)
+    return contacts
+
+
+@app.get('/{contact_id}', response_model=ContactResponse)
+async def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    contact = await repository_contacts.get_contact(contact_id, db)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
+    return contact
+
+
+@app.post("/", response_model=ContactResponse)
+async def create_contact(body: ContactResponse, db: Session = Depends(get_db)):
+    return await repository_contacts.create_contact(body, db)
+
+
+@app.put("/{contact_id}", response_model=ContactResponse)
+async def update_contact(body: ContactResponse, contact_id: int, db: Session = Depends(get_db)):
+    contact = await repository_contacts.update_contact(contact_id, body, db)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
+    return contact
+
+
+@app.delete("/{contact_id}", response_model=ContactResponse)
+async def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    contact = await repository_contacts.delete_contact(contact_id, db)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found")
+    return contact
+
+
+@router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def signup(body: UserModel, db: Session = Depends(get_db)):
+    exist_user = await repository_users.get_user_by_email(body.email, db)
+    if exist_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists")
+    body.password = auth_service.get_password_hash(body.password)
+    new_user = await repository_users.create_user(body, db)
+    return {"user": new_user, "detail": "User successfully created"}
+
+
+@router.post("/login", response_model=TokenModel)
+async def login(body: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = await repository_users.get_user_by_email(body.username, db)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
+    if not auth_service.verify_password(body.password, user.password):
+        raise  HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
+
+    access_token = await auth_service.create_access_token(data={"sub": user.email})
+    refresh_token = await auth_service.create_refresh_token(data={"sub": user.email})
+    await repository_users.update_token(user, refresh_token, db)
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+
+@router.get("/refresh_token", response_model=TokenModel)
+async def refresh_token(credentials: HTTPAuthorizationCredentials = Security(security), db: Session = Depends(get_db)):
+    token = credentials.credentials
+    email = await auth_service.decode_refresh_token(token)
+    user = await repository_users.get_user_by_email(email, db)
+    if user.refresh_token != token:
+        await repository_users.update_token(user, None, db)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
+    access_token = await auth_service.create_access_token(data={"sub": email})
+    refresh_token = await auth_service.create_refresh_token(data={"sub": email})
+    await repository_users.update_token(user, refresh_token, db)
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
